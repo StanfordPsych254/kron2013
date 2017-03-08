91a0cd6a05ce84b36448662a40e47a8caed85455
Maia ten Brink <maia.tenbrink@gmail.com>
2017-03-07 22:29:56 -0800

    3/7/17 working on fixing first experimental trial

--- mturk files/kron-affect-rating.js
+++ mturk files/kron-affect-rating.js
@@ -1,15 +1,8 @@
-// 3/5/17: Outstanding issues to fix:
-// 1. images have to scroll down to see whole thing; not fixed by setting height to 100% or auto
-// 2. possible to click through ratings without having made a response
-// 3. sometimes "please make a response" appears without warning
-// 4. iaps_trial is not being response_logged
-// 5. create pseudorandom order of trials within subblocks, then randomize order of subblocks
-// 6. sometimes one of the radio buttons is already clicked (especially when i haven't answered previous rating) = anchoring!
-// 7. try forcing it into fullscreen...
-// 8. run through whole experiment to see if demographics at end are working and data collected
-// 9. optional: enter normative IAPS ratings so data is easier to deal with
-// 10. checkmobile is not functioning properly, not being logged
-// 11. subblock is counting up too high during the practice trials
+// 3/6/17: Outstanding issues to fix:
+// create pseudorandom order of trials within subblocks, then randomize order of subblocks
+// optional: enter normative IAPS ratings so data is easier to deal with
+// finished function is timing out too quickly
+// stop it from counting position in the instructions3 (begin) slide
 
 
 // ################################ Overview #####################################
@@ -28,7 +21,7 @@
 
 
 // To do practice: copy rest of experiment
-// append practice photos onto IAPS length - append, say if 3 then done
+// append practice photos onto IAPS length - append, say if 3* (or however many) then done
 
 // ############################## Helper functions ##############################
 
@@ -58,7 +51,7 @@ Array.prototype.random = function() {
 }
 
 // shuffle function - from stackoverflow?
-// shuffle ordering of argument array -- are we missing a parenthesis?
+// shuffle ordering of argument array
 function shuffle (a)
 {
     var o = [];
@@ -84,12 +77,13 @@ $.urlParam = function(name){
 }
 
 //Check if using a mobile device
+  //from: http://stackoverflow.com/questions/3514784/what-is-the-best-way-to-detect-a-mobile-device-in-jquery
 var checkmobile = false;
 var mobilecheck = function() {
   (function(a){
     if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) checkmobile = true;})
     (navigator.userAgent||navigator.vendor||window.opera);
-   return checkmobile; // This isn't working!
+   return checkmobile;
 };
 
 // Set scale to show
@@ -165,6 +159,10 @@ var showRating = function() {
   } 
 }
 
+var subtractPosition = function() {
+  part_of_trial = -1;
+}
+
 var countPosition = function() {
   // Count part_of_trial: 0 = picture, 1 = 1st rating scale, 2 = 2nd rating scale
   if (part_of_trial == 2) {  // if 2nd rating has been completed, start at top of next trial
@@ -179,9 +177,7 @@ var countPosition = function() {
   }
 
   // Count subblocks and blocks from start of experiment (i.e. trials - 4)
-  if (is_practice == false) {
-    var subblock = 1;
-    var block = 1; 
+  if (is_practice == false) { 
 
     // Count subblock number (1:4) within subblock
     // skipping ahead of first 4 practice trials, if 6th trial in subblock, subblock ends
@@ -211,7 +207,7 @@ var iaps = [
 // 1052,1200,1303,1450,1460, //5
 // 1540,1645,1661,1710,1931, //10
 // 2091,2190,2205,2210,2300, //15
-// 2304,2347,2352.2,2358,2530, //2
+// 2304,2347,2352.2,2358,2530, //20
 // 2580,2590,2595,2682,2688, //25
 // 2770,2900.1,3000,3053,3063, //30
 // 3180,3185,4225,4274,4490, //35
@@ -221,14 +217,14 @@ var iaps = [
 // 7046,7078,7175,7234,7440, //55
 // 7476,7545,8065,8160,8185, //60
 // 8190,8200,8311,8312,8400, //65
-8475,8540,9000,9332,9412, //70
+ 8475,8540,9000,9332,9412, //70
 9471,9594 //72
 ];
 
 var totalTrials = iaps.length;
 
 function getiapsFile(image_num) {
-  return 'http://web.stanford.edu/~mtb/IAPS_stimuli/' + image_num + '.jpg';
+  return 'http://web.stanford.edu/~mtb/IAPS_stimuli/' + image_num + '.jpg'; //images stored on my website
 }
 
 // Initialize rating scale order for entire experiment (50% chance)
@@ -380,14 +376,15 @@ if (present_order == 8) {
 
 // Set counts for trial, subblock, block, and part_of_trial
 var trial = 1;
-var subblock = 0;
-var block = 0;
+var subblock = 1;
+var block = 1;
 var part_of_trial = 0; // make sure this updates!
 var new_subblock = [];
 var new_block = [];
 var iaps_trial;
 var scale_content;
 
+var date = new Date();
 // ############################## Instructions ##################################
 
 // Show the instructions slide -- this is what we want subjects to see first.
@@ -400,7 +397,7 @@ var is_practice = false;
 var practice_iaps = [
 // 5010,
 // 4005,
-1820,
+// 1820,
 2320
 ];
 
@@ -425,24 +422,34 @@ var experiment = {
 
     // The object to be submitted.
     data: {
+      //trial info
       iaps_order: [],
-      iaps_trial: [],
       scale_order: [],
       scale_type: [],
       scale_content: [],
+      //where we are in trials
       is_practice: [],
-      rating: [],
-      elapsed_ms: [],
-      user_agent: [],
+      part_of_trial: [],
       trial: [],
       subblock: [],
       block: [],
       new_subblock: [],
       new_block: [],
-      part_of_trial: [],
+      //trial data
+      iaps_trial: [],
+      rating: [],
+      elapsed_ms: [],
+      //browser checks
       window_width: [],
       window_height: [],
-      checkmobile: []
+      checkmobile: [],
+      //user and demographics
+      user_agent: [],
+      date: [],
+      age: [],
+      gender: [],
+      education: [],
+      chronotype: []
     },
 
     start_ms: 0,  // time current trial started ms
@@ -463,7 +470,7 @@ var experiment = {
       var elapsed = Date.now() - experiment.start_ms;
 
       //Array of radio buttons
-      var radio = document.getElementsByName("judgment");   // This isn't working properly
+      var radio = document.getElementsByName("judgment");
 
       // Loop through radio buttons
       for (i = 0; i < radio.length; i++) {
@@ -500,7 +507,6 @@ var experiment = {
           radio[i].checked = false
         }
 
-        //$('#stage-content').hide(); // check this
         $(".testMessage").html('');   // clear the test message
         showRating();
       } else {
@@ -519,6 +525,7 @@ var experiment = {
               String(100 * (1 - iaps_order.length/totalTrials)) + "%")
           // style="width:progressTotal%"
 
+          // practice trials
           if (is_practice == true) {
 
             // Get the current trial - <code>shift()</code> removes the first element
@@ -535,7 +542,6 @@ var experiment = {
 
             //display slides:
             showSlide("stage");
-            //$("#nomobile-content-content").hide();
             $("#bipolarvalence-content").hide();
             $("#arousal-content").hide();
             $("#pleasant-content").hide();
@@ -567,7 +573,8 @@ var experiment = {
             }
 
           } 
-          else { // experimental trials
+          // experimental trials
+          else { 
 
             // Get the current trial - <code>shift()</code> removes the first element
             // select from our scales array and stop exp after we've exhausted all the domains
@@ -589,7 +596,6 @@ var experiment = {
 
             //display slides:
             showSlide("stage");
-            //$("#nomobile-content-content").hide();
             $("#bipolarvalence-content").hide();
             $("#arousal-content").hide();
             $("#pleasant-content").hide();
@@ -620,6 +626,9 @@ var experiment = {
     //  go to debriefing slide
     debriefing: function() {
       showSlide("debriefing");
+      setTimeout(function() {
+      $("#debriefing").show();
+      }, 8000);
     },
 
     // submitcomments function
@@ -640,10 +649,15 @@ var experiment = {
       experiment.data.age.push(document.getElementById("age").value);
       experiment.data.gender.push(document.getElementById("gender").value);
       experiment.data.education.push(document.getElementById("education").value);
+      experiment.data.chronotype.push(document.getElementById("chronotype").value);
       experiment.data.expt_aim.push(document.getElementById("expthoughts").value);
       experiment.data.expt_gen.push(document.getElementById("expcomments").value);
       experiment.data.type.push(type);
       experiment.data.user_agent.push(window.navigator.userAgent);
+      // Save date
+      experiment.data.date.push(date);
+
+      // End experiment
       experiment.end();
     }
 }
